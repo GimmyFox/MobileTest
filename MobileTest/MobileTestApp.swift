0b1f770215fdcfd112174230bb10ca8b5468d045
GimmyFox <mguzeev99@gmail.com>
2023-12-19 17:12:44 +0500

    Initial Commit

---
+++ MobileTest/MobileTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileTestApp.swift
+//  MobileTest
+//
+//  Created by Maksim Guzeev on 19.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct MobileTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
